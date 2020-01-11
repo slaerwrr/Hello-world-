a14854aaee90d6192d22d32ea4d87267730657bc
slaerwrr <aljdljh@naver.com>
2020-01-11 17:02:47 +0900

    char
    char 예시

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 
-#pragma region for,if
+ #pragma region for,if
 	//int a = 10;
 	//int b = 20;
 /*
@@ -38,10 +38,10 @@ int main()
 	*/
 
 	//for (int i = 0; i < 10; i++)
-	{
+	//{
 		// c++에서 출력하는 방법
 		//cout << i << endl;
-		// c에서 출려가는 방법
+		// c에서 출력하는 방법
 		//이스케이프 문자를 알아야 한다.
 		// % + 출력할 문자
 		// int = %d
@@ -53,30 +53,14 @@ int main()
 		//다르면 NO! 라고 출력
 		
 
-	}
+	//}
 
 
-#pragma region for,if,end
 
-	int result = 0;
-	int input = 0;
+
 	
 
-	while(result < 10000)
-	{
-		cin >> input;
-		
-		if (input == 7)
-		{
-			break;
-		}
-		result = result + input;
-		
-		cout << " 내가 작성한 값 :" << input << endl;
-
-		cout << "총합 :" << result << endl;
-
-	}
+	
 	
 
 	// 값을 받고 해당 값이 10000을 넘으면 종료시키는 프로그램 제작
@@ -85,6 +69,75 @@ int main()
 	// 만약 result 값이 10000보다 크면 종료해준다.(return 0)
 	//아니라면 다시 1번으로 돌아간다.
 
+	
+
+		
+
+#pragma endregion
+
+
+	// switch ?
+
+	/*int input = 0;
+	cin >> input;
+
+	switch (input)
+	{
+
+	case 0:
+		cout << "해당값은 0입니다" << endl;
+		break;
+
+	case 1:
+
+		cout << "해당값은 1입니다" << endl;
+		break;
+
+	case 2:
+
+		cout << "해당 값은 2 입니다." << endl;
+
+		break;
+	default:
+		cout << " 값을 확인 할 수 없습니다." << endl;
+		break;
+	}
+	*/
+
+#pragma region Array, Char
+	// char == 1 byte
+
+/*
+int numbers[] = { 0,1 };
+
+char c[] = "Hello World!";
+
+for (int i = 0; i < 13; i++)
+{
+	if (i == 5)
+	{
+		continue;
+	}
+
+	printf("%c\n", c[i]);
+}
+
+int length = _countof(c);
+*/
+// c++
+//cout << c << endl;
+//cout << c[2] << endl;
+// c
+//printf("%s\n ", c);
+//printf("%c\n", c[0]);
+
+char str[256] = { 0 , };
+
+//cin >> str
+
+scanf_s("%s", str, 256);
+printf("%s" , str[\0]);
+#pragma endregion
 
 	return 0;
 }
